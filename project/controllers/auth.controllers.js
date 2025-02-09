030663703bc074435974f9288cb774c79b13ba00
samfip1 <samantrathod6@gmail.com>
2025-02-09 12:07:27 +0530

    Added signup auth

--- project/controllers/auth.controllers.js
+++ project/controllers/auth.controllers.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import User from "../models/user.model";
-
+import { bcrypt } from "bcryptjs";
+const jwt = require('jsonwebtoken')
 // req -> it is body which comes from the user
-
+const jwt_secret = "349805ytrghwu67894tg5uwei9ghwe"
 export const singup = async (req, res) => {
     const sesson = await mongoose.startSession();   // start session
     sesson.startTransaction();  // start transaction
@@ -13,10 +14,36 @@ export const singup = async (req, res) => {
 
         const existingUser = await User.findOne({email});
         if(existingUser) {
-
+            const error = new Error('User already exist');
+            error.statusCode = 409;
+            throw error;
         }
 
+        // hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create(
+            [
+                {name , email, password: hashPassword}
+            ],
+            {sesson}
+        )
+
+        const token = jwt.sign({userId : newUser[0]._id}, jwt_secret);
+
+
         await sesson.commitTransaction();
+        sesson.endSession();
+
+        res.status(201).json({
+            success: true,
+            message : 'USer successfully created',
+            data : {
+                token,
+                user: newUser[0],
+            }
+        })
     } catch (error) {
         await sesson.abortTransaction();
         sesson.endSession();
@@ -27,6 +54,7 @@ export const singup = async (req, res) => {
 export const signin = async (req, res) => {
 
     
+
 }
 
 export const signout = async (req, res) => {
